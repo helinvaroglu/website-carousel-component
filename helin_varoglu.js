7c07a89a3750d15fd5f2d2ef0f28e6e999f4e903
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-03-18 11:20:20 +0300

    Added header

--- helin_varoglu.js
+++ helin_varoglu.js
@@ -1,14 +1,15 @@
 (() => {
     const init = () => {
-        self.buildHTML();
-        self.buildCSS();
-        self.setEvents();
+        buildHTML();
+        buildCSS();
+        setEvents();
     };
 
     const buildHTML = () => {
         const html = `
             <div class="container">
-                <h1></h1>
+                <h1>You Might Also Like</h1>
+                
             </div>
         `;
 
@@ -18,10 +19,21 @@
     const buildCSS = () => {
         const css = `
             .container {
-                background-color: red;
-                height: 100px;
-                width: 100px;
+                background-color: transparent;
+                position: relative;
+                padding: 0 0;
             }
+            
+            h1 {
+                font-size: 24px;
+                color: #29323b;
+                line-height: 33px;
+                font-weight: lighter;
+                padding: 15px 0;
+                margin: 0;
+            }
+
+            
         `;
 
         $('<style>').addClass('carousel-style').html(css).appendTo('head');
